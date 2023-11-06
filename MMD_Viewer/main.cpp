b86868285b162ef2d2a337067b6ae5df350189bb
rono0920 <rono0920@gmial.com>
2023-11-06 15:35:03 +0900

    Revert "テクスチャ読み込み試作２"
    This reverts commit 27f4dd2b944464dd16d47cad8fd3c4098b7aed0c.

--- MMD_Viewer/main.cpp
+++ MMD_Viewer/main.cpp
@@ -33,52 +33,51 @@ void LoadAndCout(const char* filepath)
 	MMDsdk::PmdFile model(filepath);
 	//DebugMessage(GetText(model.GetToonTexturePath(0)));
 
-	for (int k = 0; k < 10; ++k)
+	for (int k = 0; k < model.GetMaterialCount(); ++k)
 	{
-		auto& texpath = model.GetToonTexturePath(k);
-		
-		DebugOutString(GetText(texpath));
-		
-		int slashCount = 0;
-		for (int i = 0; filepath[i] != '\0'; ++i)
+		if (model.GetMaterial(k).texturePath.GetFirstChar() != '\0')
 		{
-			if (filepath[i] == '/')
+			model.DebugOutMaterial(k);
+			auto& texpath = model.GetMaterial(k).texturePath;
+			DebugOutString(GetText(texpath));
+			int slashCount = 0;
+			for (int i = 0; filepath[i] != '\0'; ++i)
 			{
-				++slashCount;
-			}
-		}
-
-		int slashCount2 = 0;
-		char* modelAssetPath = nullptr;
-		
-		for (int i = 0; filepath[i] != '\0'; ++i)
-		{
-			//DebugOutArray(filepath, i);
-			if (filepath[i] == '/')
-			{
-				++slashCount2;
-				if (slashCount == slashCount2)
+				if (filepath[i] == '/')
 				{
-					// “Y‚¦Žš‚©‚ç•¶Žš—ñ’·‚³‚ð‹‚ß‚éB1Žn‚Ü‚è‚ÉC³
-					int dirLength = i + 1;
-					modelAssetPath = new char[dirLength + texpath.GetLength()];
-					for (int j = 0; j < dirLength; ++j)
-					{
-						modelAssetPath[j] = filepath[j];
-					}
-					for (int j = 0; j < texpath.GetLength(); ++j)
-					{
-						modelAssetPath[j + dirLength] = (GetText(texpath))[j];
-					}
-					break;
+					++slashCount;
 				}
 			}
-		}
-		
-		DebugMessage(modelAssetPath);
-		FileReadBin file(modelAssetPath);
+			int slashCount2 = 0;
+			char* modelAssetPath = nullptr;
+			for (int i = 0; filepath[i] != '\0'; ++i)
+			{
+				//DebugOutArray(filepath, i);
+				if (filepath[i] == '/')
+				{
+					++slashCount2;
+					if (slashCount == slashCount2)
+					{
+						// “Y‚¦Žš‚©‚ç•¶Žš—ñ’·‚³‚ð‹‚ß‚éB1Žn‚Ü‚è‚ÉC³
+						int dirLength = i + 1;
+						modelAssetPath = new char[dirLength + texpath.GetLength()];
+						for (int j = 0; j < dirLength; ++j)
+						{
+							modelAssetPath[j] = filepath[j];
+						}
+						for (int j = 0; j < texpath.GetLength(); ++j)
+						{
+							modelAssetPath[j + dirLength] = (GetText(texpath))[j];
+						}
+						break;
+					}
+				}
+			}
+			DebugMessage(modelAssetPath);
+			FileReadBin file(modelAssetPath);
 
-		SafeDeleteArray(&modelAssetPath);
+			SafeDeleteArray(&modelAssetPath);
+		}
 	}
 }
 
