db56f9018108a54f50621e7fc77f0b9a6732304b
rono0920 <rono0920@gmial.com>
2023-11-27 16:31:03 +0900

    微修正

--- Direct3D12_Learning/main.cpp
+++ Direct3D12_Learning/main.cpp
@@ -362,13 +362,15 @@ DirectX::ScratchImage scratchImg = {};
 static const char* const kabanPath = "../x64/Debug/Test/Model/PMX/かばんちゃん/かばんちゃん/かばんちゃん.pmx";
 static const char* const hashibiroPath = "../x64/debug/PMX/ハシビロコウ/ハシビロコウ.pmx";
 static const char* const stagePath = "../x64/debug/Test/Model/PMX/キョウシュウエリアver1.0/キョウシュウエリア/キョウシュウエリア20170914.pmx";
-static const char* const mikuPath = "../x64/Debug/Test/Model/PMD/初音ミクVer2.pmd";
+static const char* const mikuPath = "../x64/Debug/Test/Model/PMD/初音ミク.pmd";
+static const char* const miku2Path = "../x64/Debug/Test/Model/PMD/初音ミクVer2.pmd";
 static const char* const meikoPath = "../x64/Debug/Test/Model/PMD/MEIKO.pmd";
 static const char* const kaitoPath = "../x64/Debug/Test/Model/PMD/カイト.pmd";
 static const char* const rinPath = "../x64/Debug/Test/Model/PMD/鏡音リン.pmd";
+static const char* const rukaPath = "../x64/Debug/Test/Model/PMD/巡音ルカ.pmd";
 
 const MMDsdk::PmxFile model(kabanPath);
-const MMDsdk::PmdFile miku(mikuPath);
+const MMDsdk::PmdFile miku(rukaPath);
 
 auto gMatrix = DirectX::XMMatrixIdentity();
 auto gWorld = DirectX::XMMatrixIdentity();
