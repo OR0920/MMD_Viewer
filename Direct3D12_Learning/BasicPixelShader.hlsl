e8e5c06d1169033989f3da5942a601949ca6dfe3
OR0920 <rono0920@gmail.com>
2023-11-28 11:34:23 +0900

    シェーダー変更

--- Direct3D12_Learning/BasicPixelShader.hlsl
+++ Direct3D12_Learning/BasicPixelShader.hlsl
@@ -3,15 +3,29 @@
 
 float4 BasicPS(VS_OutPut vso) : SV_TARGET
 {
-    float3 light = normalize(float3(1.f, -1.f, 1.f));
+	float3 light = normalize(float3(1.f, -1.f, 1.f));
 
-    float brightness = dot(-light, vso.normal.xyz);
-    
-    float4 col;
-    col.xyz = brightness;
-    col.w = 1.f;
-    col *= diffuse;
-    col *= tex.Sample(smp, vso.uv);
-    return col;
+	float brightness = dot(-light, vso.normal.xyz);
+
+	//if (brightness > 0.5)
+	//{
+	//	brightness = 1;
+	//}
+	//else if(brightness > 0.1)
+	//{
+	//	brightness = 0.75;
+	//}
+	//else
+	//{
+	//	brightness = 0.5;
+	//}
+
+
+	float4 col;
+	col.xyz = brightness;
+	col.w = 1.f;
+	col *= diffuse;
+	col *= tex.Sample(smp, vso.uv);
+	return col;
 }
 
