1f247137ac1fbb94457d50dc0ae0cd0d278d4c5b
rono0920 <rono0920@gmial.com>
2024-03-06 15:21:31 +0900

    不要なプロジェクトを削除

--- Direct3D12_Learning/StringUtil.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void newArray_GetExtention(char** extPtr, const char* const filename);
-
-bool newArray_SplitFileName(char** primaryTex, char** secondaryTex, const char* const filename);
--- Direct3D12_Learning/UsingComPtr.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include<WRL.h>
-template<class C>
-using ComPtr = Microsoft::WRL::ComPtr<C>;
