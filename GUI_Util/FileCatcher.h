9aec21a73b135e355f5525240b6307d46670fa2c
rono0920 <rono0920@gmial.com>
2024-04-23 15:35:08 +0900

    FileCatcher　ヘッダー分離

---
+++ GUI_Util/FileCatcher.h
@@ -0,0 +1,51 @@
+#ifndef _FILE_CATCHER_H_
+#define _FILE_CATCHER_H_
+
+namespace GUI_Util
+{
+
+	// 領域内にドロップされたファイルを認識するウィンドウ
+	class FileCatcher
+	{
+	public:
+		FileCatcher(); ~FileCatcher();
+
+		// 親を指定し生成
+		// 親のクライアント領域いっぱいにサイズが指定される
+		// 親のサイズ変更に合わせて自動で調整される
+		Result Create(const ParentWindow& parent);
+
+		bool Update();
+
+		// パスの長さ、パス、ドロップされた位置を取得する
+		int GetLength() const;
+		int GetWideLength() const;
+		const char* const GetPath() const;
+		const wchar_t* const GetWidePath() const;
+
+		// ファイルがドロップされた場所
+		struct DropPos
+		{
+			long x;
+			long y;
+		};
+		const DropPos& GetDropPos() const;
+
+	private:
+		FileCatcher(const FileCatcher&) = delete;
+		const FileCatcher& operator=(const FileCatcher&) = delete;
+
+
+		std::string mFilePath;
+		bool mIsUpdated;
+		wchar_t mWideFilePath[MAX_PATH];
+		DropPos mDropPos;
+
+
+		// このコールバック関数に、データを更新してもらう。
+		static LRESULT CALLBACK FileCatcherProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp);
+		static WNDCLASSEX mWindowClass;
+	};
+}
+
+#endif // !_FILE_CATCHER_H_
--- GUI_Util/GUI_Util.h
+++ GUI_Util/GUI_Util.h
@@ -29,6 +29,7 @@
 
 #include"ParentWindow.h"
 #include"MainWindow.h"
+#include"FileCatcher.h"
 
 // WIN32APIのウィンドウ周りをラップ
 namespace GUI_Util
@@ -36,48 +37,9 @@ namespace GUI_Util
 	// エラー表示用ダイアログボックス
 	void ErrorBox(const TCHAR* const message);
 
-	// 領域内にドロップされたファイルを認識するウィンドウ
-	class FileCatcher
-	{
-	public:
-		FileCatcher(); ~FileCatcher();
-
-		// 親を指定し生成
-		// 親のクライアント領域いっぱいにサイズが指定される
-		// 親のサイズ変更に合わせて自動で調整される
-		Result Create(const ParentWindow& parent);
-
-		bool Update();
-
-		// パスの長さ、パス、ドロップされた位置を取得する
-		int GetLength() const;
-		int GetWideLength() const;
-		const char* const GetPath() const;
-		const wchar_t* const GetWidePath() const;
-
-		// ファイルがドロップされた場所
-		struct DropPos
-		{
-			long x;
-			long y;
-		};
-		const DropPos& GetDropPos() const;
-
-	private:
-		FileCatcher(const FileCatcher&) = delete;
-		const FileCatcher& operator=(const FileCatcher&) = delete;
-
-
-		std::string mFilePath;
-		bool mIsUpdated;
-		wchar_t mWideFilePath[MAX_PATH];
-		DropPos mDropPos;
-
-
-		// このコールバック関数に、データを更新してもらう。
-		static LRESULT CALLBACK FileCatcherProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp);
-		static WNDCLASSEX mWindowClass;
-	};
+	class ParentWindow;
+	class MainWindow;
+	class FileCatcher;
 }
 
 #endif // !_GUI_UTIL_H_
